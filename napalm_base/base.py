2f8d698439afee758e335d9b46d04c9c379d3e66
David Barroso <dbarrosop@dravetech.com>
2017-01-28 19:39:34 +0100

    Added POC to translate from OC to native config

--- napalm_base/base.py
+++ napalm_base/base.py
@@ -1567,3 +1567,46 @@ class NetworkDriver(object):
             attr(value)
         except ValueError:
             attr(eval(value))
+
+    def translate_yang_model(self, model):
+        with open("napalm_eos/openconfig_translation.yaml", "r") as f:
+            translation_map = yaml.load(f.read())["interfaces"]
+
+        return self._translate_yang_model(model, translation_map)
+
+    def _translate_yang_model(self, model, translation_map):
+        translation = ""
+        for k, v in model.items():
+            if issubclass(v.__class__, napalm_yang.List):
+                translation += self._yang_translate_list(v, translation_map[k])
+            else:
+                if issubclass(v.__class__, napalm_yang.BaseBinding):
+                    if not v._meta["config"]:
+                        continue
+                    translation += self._translate_yang_model(getattr(model, k), translation_map[k])
+                translation += self._yang_translate_string(translation_map[k].get("_string", ""),
+                                                           value=getattr(model, k))
+                translation += self._yang_translate_map(translation_map[k].get("_map", {}),
+                                                        value=getattr(model, k))
+        return translation
+
+    def _yang_translate_list(self, l, translation_map):
+        translation = ""
+        for element_key, element_data in l.items():
+            translation += self._yang_translate_string(translation_map.get("_string", ""),
+                                                       key=element_key)
+            translation += self._translate_yang_model(element_data, translation_map)
+
+        return translation
+
+    def _yang_translate_map(self, m, value, **kwargs):
+        if m:
+            return self._yang_translate_string(m[str(value)]["_string"], value=value, **kwargs)
+        else:
+            return ""
+
+    def _yang_translate_string(self, string, **kwargs):
+        if string:
+            return string.format(**kwargs)
+        else:
+            return ""
